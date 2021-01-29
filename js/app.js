6f90dde6609dbb2bae8a0e013fa3b155e1b956ac
Mayank Gupta <mayank.pkgupta@outlook.com>
2021-01-30 01:59:25 +0530

    First commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    // $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.materialboxed').materialbox();
+    // $('.datepicker').datepicker({
+    //   disableWeekends: true,
+    //   yearRange: 1
+    // });
+    $('.tooltipped').tooltip();
+     $('.scrollspy').scrollSpy();
+
+  });
